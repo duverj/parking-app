26e0b7c18f8c7c784f77995aad1ec7a62990833d
Victor Duarte <vicsalf@hotmail.com>
2015-01-13 00:03:57 -0500

    - Modified gulp build script to uglify the js files. - Added public folder to the gitignore list.

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,18 @@
 var src = './src';
 var dist = './public';
 var gulp = require('gulp');
-var concat = require('gulp-concat-sourcemap');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 /**
  * Scripts setup
  */
 gulp.task('build', function () {
   gulp.src(src + '/javascripts/*.js')
-    .pipe(concat('all.min.js', {
-      sourcesContent: true,
-      sourceRoot: "./"
-    }))
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(dist + '/javascripts/'));
 });
