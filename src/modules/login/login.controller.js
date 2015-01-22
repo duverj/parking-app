e108952b4b993711e6f51184f73fda4691adb678
Victor Duarte <vicsalf@hotmail.com>
2015-01-21 23:04:44 -0500

    Adding documentation

--- src/modules/login/login.controller.js
+++ src/modules/login/login.controller.js
@@ -4,10 +4,16 @@
 login.controller('loginCtrl', function ($state) {
   this.methods = {};
 
+  /**
+   * Cta for user submitting the login form
+   */
   this.methods.submit = function () {
     console.log('user submitted login');
   };
 
+  /**
+   * Cta for user cancelling the login form
+   */
   this.methods.cancel = function () {
     console.log('user cancelled login');
   };
