aa5ef5525103e997597c5a73ab509843b01f3d44
Victor Duarte <vicsalf@hotmail.com>
2015-01-21 23:03:16 -0500

    Login module, base styling. And simple controller/template interaction.

--- src/modules/login/login.controller.js
+++ src/modules/login/login.controller.js
@@ -1,6 +1,14 @@
 // Login controller
 'use strict';
 
-login.controller('loginCtrl', function () {
-  this.title = 'login page';
+login.controller('loginCtrl', function ($state) {
+  this.methods = {};
+
+  this.methods.submit = function () {
+    console.log('user submitted login');
+  };
+
+  this.methods.cancel = function () {
+    console.log('user cancelled login');
+  };
 });
