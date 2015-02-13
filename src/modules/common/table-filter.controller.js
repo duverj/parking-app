f44d3ea6ce123fd10125dcd1bf93ac64b889e277
FelipeAG06 <felipeag06@gmail.com>
2015-02-13 16:07:00 -0500

    2 spaces indentation added

--- src/modules/common/table-filter.controller.js
+++ src/modules/common/table-filter.controller.js
@@ -2,17 +2,14 @@
 'use strict';
 
 common.controller('tableFilterCtrl', function ($log , ngDialog) {
-	var self = this;
+  var self = this;
 
-	this.newVehicle = function() {
-		ngDialog.open({
-			template:'views/common/new-vehicle.html',
-			className: 'modal-container',
-			controller: self
-		});
-	};
+  this.newVehicle = function() {
+    ngDialog.open({
+      template:'views/common/new-vehicle.html',
+      className: 'modal-container',
+      controller: self
+    });
+  };
 
-	this.saveNew = function() {
-		console.log('saving!!');
-	};
 });
